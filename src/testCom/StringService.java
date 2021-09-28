9ede94f3dbe4443aba0e2e69d63b887460803d49
Egor Novichkov <61648756+spelldone@users.noreply.github.com>
2021-09-28 21:11:43 +0400

    Initial commit

---
+++ src/testCom/StringService.java
@@ -0,0 +1,11 @@
+package testCom;
+public  class StringService {
+
+    public boolean checkForContainingSubstring(String line, String substring){
+        return line.contains(substring);
+    }
+}
+
+
+
+
